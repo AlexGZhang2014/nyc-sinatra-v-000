a115217f39ecea86f1a5b85623b7b3b346bcbce7
Alex Zhang <alexgzhang92@gmail.com>
2018-06-28 01:19:18 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,13 @@
 class FiguresController < ApplicationController
   get '/figures' do
     @figures = Figure.all
-    erb :'/figures/index'
+    erb :'figures/index'
   end
+  
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+  
+  post '/figures' do
+    
 end
